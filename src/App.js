c3c6adae553a21b6fe6fe5d139a1a13cbba99551
triciasykes <tlsykes98@gmail.com>
2023-09-13 15:43:35 -0700

    original app configs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 import mockApartments from "./mockApartments"
 import mockUsers from "./mockUsers"
+import "./App.css"
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(mockUsers[0])
+  const [apartments, setApartments] = useState(mockApartments)
   return (
     <>
-      <h1>Helooooooo</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
   )
 }
