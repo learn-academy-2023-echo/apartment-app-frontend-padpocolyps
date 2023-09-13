5912fcf355b6c70567707815fadbd963f4e3caac
Elyse Montano <elysemontano.dev.com>
2023-09-13 16:35:40 -0700

    adds files and folders, navigation, basic code in componets, index, and some styling

---
+++ src/pages/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="not-found-background">
+        <div className="not-found-text">
+          <h1>404</h1>
+          <h3>Oops, could not find that place</h3>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NotFound
