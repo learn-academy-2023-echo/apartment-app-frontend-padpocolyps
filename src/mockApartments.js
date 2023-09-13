76196cf636d56fd872ae4a5994ca01fd256e6d17
triciasykes <tlsykes98@gmail.com>
2023-09-13 13:30:24 -0700

    react configs complete

---
+++ src/mockApartments.js
@@ -0,0 +1,34 @@
+let mockApartments = [
+  {
+    id: 1,
+    street: "Cantebury Lane",
+    unit: "43",
+    city: "Phoenix",
+    state: "AZ",
+    square_footage: 2000,
+    price: "5000",
+    bedrooms: 3,
+    bathrooms: 2.5,
+    pets: "yes",
+    image:
+      "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXBhcnRtZW50fGVufDB8fDB8fHww&w=1000&q=80",
+    user_id: 1,
+  },
+  {
+    id: 2,
+    street: "Lincoln Circle",
+    unit: "2310",
+    city: "Phoenix",
+    state: "AZ",
+    square_footage: 2500,
+    price: "5000",
+    bedrooms: 3,
+    bathrooms: 2.5,
+    pets: "yes",
+    image:
+      "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXBhcnRtZW50fGVufDB8fDB8fHww&w=1000&q=80",
+    user_id: 2,
+  },
+]
+
+export default mockApartments
